af9aefb270105788c040ab5d5b64b5b3e276d605
Nick Darlow <dangerous.darlow@gmail.com>
2020-05-29 23:04:27 +0100

    new project created with wizard

---
+++ Queue/Controllers/QueueController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Queue.Controllers
+{
+    [ApiController]
+    [Route("queue")]
+    public class QueueController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "hello";
+        }
+    }
+}
